554b723d600bf9e540c7de3ab3353db32eb4ed6b
avneesh atri <atri.avneesh@gmail.com>
2021-12-20 19:17:19 +0530

    refactoring

--- annpurna-zudexo-cli/src/test/java/org/annpurna/cli/walle/TestZudexoWalletContractService.java
+++ annpurna-zudexo-cli/src/test/java/org/annpurna/cli/walle/TestZudexoWalletContractService.java
@@ -1,19 +1,23 @@
 package org.annpurna.cli.walle;
 
+import org.annpurna.cli.identity.AddToIdentityWallet;
 import org.annpurna.cli.wallet.ZudexoWalletContractService;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestZudexoWalletContractService {
+	private AddToIdentityWallet walletService ;
 	private ZudexoWalletContractService contractService ;
 	
 	@Before
 	public void setUp() {
 		this.contractService = new ZudexoWalletContractService();
+		this.walletService = new AddToIdentityWallet();
 	}
 
 	@Test
 	public void test() {
+		this.walletService.main(null);
 		this.contractService.main(null);
 	}
 }
